27651fca4842f14f49974e10b8dd8965d5f2ae6a
Ryan Thompson <service@aiwebsystems.com>
2021-10-13 18:57:44 -0500

    Upstairs

--- src/index.tsx
+++ src/index.tsx
@@ -8,43 +8,6 @@ import App from './components/App';
 
 import reportWebVitals from './reportWebVitals';
 
-let app = window.streams.core.app;
-
-    app
-        .initialize({
-            providers: [
-                window.streams.core.HttpServiceProvider,
-                window.streams.core.StreamsServiceProvider,
-                //window.app.AppServiceProvider
-            ],
-            config: {
-                http: {
-                    //baseURL: this.env.get('APP_URL', 'http://localhost') + '/' + this.env.get('STREAMS_API_PREFIX', 'api'),
-                    baseURL: 'http://127.0.0.1:8000/api',
-                }
-            },
-        })
-        .then(app => {
-
-            app.boot.bind(app);
-
-            console.log('Initialized');
-
-            return app;
-        })
-        .then(app => {
-
-            app.start();
-
-            console.log('Started');
-
-            return app;
-
-        })
-        .then(app => {
-            // Something else.
-        });
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
